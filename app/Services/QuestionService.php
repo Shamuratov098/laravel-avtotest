70111d65c1292b1ca708ad93e33f175a3e1cd403
dostonbek <shamuratov1111@gmail.com>
2026-04-10 16:50:14 +0500

    fix Question services

--- app/Services/QuestionService.php
+++ app/Services/QuestionService.php
@@ -20,31 +20,24 @@ readonly class QuestionService
      */
     public function getQuestionByCategoryId(int $id): Collection
     {
-        $category = Category::findOrFail($id);
+        Category::findOrFail($id);
 
-        try {
-            if (!$category) {
-                throw new Exception('Category not found');
-            }
-            return Question::query()
-                ->select([
+        return Question::query()
+            ->select([
+                'id',
+                'category_id',
+                'question_text',
+                'image_url',
+            ])
+            ->where('category_id', $id)
+            ->with(['answers' => function ($query) {
+                $query->select([
                     'id',
-                    'category_id',
-                    'question_text',
-                    'image_url',
-                ])
-                ->where('category_id', $id)
-                ->with(['answers' => function ($query) {
-                    $query->select([
-                        'id',
-                        'question_id',
-                        'option_number',
-                        'answer_text',
-                    ]);
-                }])
-                ->get();
-        } catch (Exception $e) {
-            Log::error($e->getMessage());
-        }
+                    'question_id',
+                    'option_number',
+                    'answer_text',
+                ]);
+            }])
+            ->get();
     }
 }
